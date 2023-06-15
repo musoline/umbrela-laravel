9913ead74329590abf3b2380db124c067ed18986
musoline <yruashvilinika@yahoo.com>
2023-06-16 03:29:20 +0400

    add product create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateProductRequest;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class ProductController extends Controller
 {
@@ -19,17 +23,21 @@ class ProductController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render("Admin/Product/Create");
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateProductRequest $request): RedirectResponse
     {
-        //
+
+
+        Product::create($request->validated());
+
+        return redirect()->route("admin/product/created");
     }
 
     /**
